d977f3ce00933d1d9f57473088d07a45dfa09add
Marcos Céspedes <mcespedeso@gmail.com>
2017-08-16 14:19:17 -0400

    Add crud endpoint user operations
    Add GET,POST,PUT,DELETE in user controller, test and swagger library in POM

---
+++ src/main/java/py/org/fundacionparaguaya/pspserver/security/user/service/UserService.java
@@ -0,0 +1,8 @@
+package py.org.fundacionparaguaya.pspserver.security.user.service;
+
+import py.org.fundacionparaguaya.pspserver.security.user.domain.User;
+import py.org.fundacionparaguaya.pspserver.util.service.CRUDService;
+
+public interface UserService extends CRUDService<User>  {
+
+}
