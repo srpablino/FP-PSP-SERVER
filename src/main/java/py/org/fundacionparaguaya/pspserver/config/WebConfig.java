5ac5627664ef41d9be2f710d1a7041709c4903d1
Rodrigo Villalba <rodris.jvo@gmail.com>
2017-08-28 17:46:26 -0400

    Add Swagger support

--- src/main/java/py/org/fundacionparaguaya/pspserver/config/WebConfig.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/config/WebConfig.java
@@ -3,17 +3,18 @@ package py.org.fundacionparaguaya.pspserver.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by rodrigovillalba on 8/25/17.
  */
 @Configuration
-@EnableWebMvc
 public class WebConfig extends WebMvcConfigurerAdapter {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**");
     }
+
 }
