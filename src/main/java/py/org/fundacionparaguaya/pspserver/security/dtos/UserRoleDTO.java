1734cc36144c65c3617d9b679fedeb316c095fa3
jaimeferreira11 <jaimeferreira11@gmail.com>
2017-09-04 09:08:51 -0400

    Contructors are added in DTO classes and unused classes are removed.
    The public and private constructors were added in the DTO classes and the unused classes are removed.

--- src/main/java/py/org/fundacionparaguaya/pspserver/security/dtos/UserRoleDTO.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/security/dtos/UserRoleDTO.java
@@ -1,15 +1,53 @@
 package py.org.fundacionparaguaya.pspserver.security.dtos;
 
+import com.google.common.base.MoreObjects;
+
 import py.org.fundacionparaguaya.pspserver.security.constants.Role;
-import py.org.fundacionparaguaya.pspserver.security.entities.UserEntity;
 
 public class UserRoleDTO {
 
 	private Long userRoleId;
 
-	private UserEntity user;
+	private UserDTO user;
 
 	private Role role;
+	
+	public UserRoleDTO(){}
+	
+	private UserRoleDTO(Long userRoleId, UserDTO user, Role role) {
+		this.userRoleId = userRoleId;
+		this.user = user;
+		this.role = role;
+	}
+	
+	public static class Builder {
+		private Long userRoleId;
+		private UserDTO user;
+		private Role role;
+		
+		public Builder userRoleId(Long userRoleId) {
+			this.userRoleId = userRoleId;
+			return this;
+		}
+		
+		public Builder user(UserDTO user) {
+			this.user = user;
+			return this;
+		}
+		
+		public Builder role(Role role) {
+			this.role = role;
+			return this;
+		}
+		
+		public UserRoleDTO build() {
+			return new UserRoleDTO(userRoleId, user, role);
+		}
+	}
+	
+	public static Builder builder() {
+		return new Builder();
+	}
 
 	public Long getUserRoleId() {
 		return userRoleId;
@@ -19,11 +57,11 @@ public class UserRoleDTO {
 		this.userRoleId = userRoleId;
 	}
 
-	public UserEntity getUser() {
+	public UserDTO getUser() {
 		return user;
 	}
 
-	public void setUser(UserEntity user) {
+	public void setUser(UserDTO user) {
 		this.user = user;
 	}
 
@@ -33,6 +71,15 @@ public class UserRoleDTO {
 
 	public void setRole(Role role) {
 		this.role = role;
+	}
+
+	@Override
+	public String toString() {
+		return MoreObjects.toStringHelper(this)
+				.add("userRoleId", userRoleId)
+				.add("user", user)
+				.add("role", role)
+				.toString();
 	}
 
 }
