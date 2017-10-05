8d9463661aaa9d65c9d3765cb8ccf7c95e7ac39d
Marcos Céspedes <mcespedeso@gmail.com>
2017-10-05 13:37:09 -0300

    Delete comments

--- src/main/java/py/org/fundacionparaguaya/pspserver/config/AuthServerOAuth2Config.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/config/AuthServerOAuth2Config.java
@@ -6,14 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.core.io.Resource;
 import org.springframework.jdbc.datasource.init.DataSourceInitializer;
-import org.springframework.jdbc.datasource.init.DatabasePopulator;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -41,12 +37,6 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
     
     @Autowired
     private DataSource dataSource;
-    
-//    @Value("classpath:schema.sql")
-//    private Resource schemaScript;
-//
-//    @Value("classpath:data.sql")
-//    private Resource dataScript;
     
     @Override
     public void configure(
@@ -91,16 +81,8 @@ public class AuthServerOAuth2Config extends AuthorizationServerConfigurerAdapter
     public DataSourceInitializer dataSourceInitializer(final DataSource dataSource) {
         final DataSourceInitializer initializer = new DataSourceInitializer();
         initializer.setDataSource(dataSource);
-        //initializer.setDatabasePopulator(databasePopulator());
         return initializer;
     }
-    
-//    private DatabasePopulator databasePopulator() {
-//        final ResourceDatabasePopulator populator = new ResourceDatabasePopulator();
-//        populator.addScript(schemaScript);
-//        populator.addScript(dataScript);
-//        return populator;
-//    }
     
     @Bean
     public TokenEnhancer tokenEnhancer() {
