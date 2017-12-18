4b124002943f749bb247c3b00651541b0670bfaf
Mirta Gonzalez <mirti.gonz@gmail.com>
2017-12-17 22:30:03 -0300

    Add service for find a family

--- src/main/java/py/org/fundacionparaguaya/pspserver/families/repositories/FamilyRepository.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/families/repositories/FamilyRepository.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import py.org.fundacionparaguaya.pspserver.families.entities.FamilyEntity;
 
 import java.lang.Long;
+import java.util.Optional;
 
 public interface FamilyRepository extends JpaRepository<FamilyEntity, Long> {
 
+	Optional<FamilyEntity> findByCode(String code);
 }
