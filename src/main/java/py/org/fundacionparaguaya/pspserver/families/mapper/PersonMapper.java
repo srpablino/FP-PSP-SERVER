d18e23e5bca5d089af00b7b82c534a70971e4f2d
Mirta Gonzalez <mirti.gonz@gmail.com>
2017-12-17 22:32:24 -0300

    Add method to convert SurveyData to Person

--- src/main/java/py/org/fundacionparaguaya/pspserver/families/mapper/PersonMapper.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/families/mapper/PersonMapper.java
@@ -1,7 +1,10 @@
 package py.org.fundacionparaguaya.pspserver.families.mapper;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
@@ -10,38 +13,58 @@ import org.springframework.stereotype.Component;
 import py.org.fundacionparaguaya.pspserver.common.mapper.BaseMapper;
 import py.org.fundacionparaguaya.pspserver.families.dtos.PersonDTO;
 import py.org.fundacionparaguaya.pspserver.families.entities.PersonEntity;
-
-
+import py.org.fundacionparaguaya.pspserver.surveys.dtos.SurveyData;
+import py.org.fundacionparaguaya.pspserver.system.entities.CountryEntity;
+import py.org.fundacionparaguaya.pspserver.system.repositories.CountryRepository;
 
 /**
- *  Created by jaimeferreira on 8/31/2017
+ * Created by jaimeferreira on 8/31/2017
  */
 @Component
 public class PersonMapper implements BaseMapper<PersonEntity, PersonDTO> {
 
 	private final ModelMapper modelMapper;
-	 
-	public PersonMapper(ModelMapper modelMapper) {
+	
+	private final CountryRepository countryRepository;
+
+	public PersonMapper(ModelMapper modelMapper, CountryRepository countryRepository) {
 		this.modelMapper = modelMapper;
+		this.countryRepository = countryRepository;
 	}
 
 	@Override
 	public List<PersonDTO> entityListToDtoList(List<PersonEntity> entityList) {
-		 return entityList.stream()
-	                .filter(Objects::nonNull)
-	                .map(this::entityToDto)
-	                .collect(Collectors.toList());
+		return entityList.stream().filter(Objects::nonNull).map(this::entityToDto).collect(Collectors.toList());
 	}
 
 	@Override
 	public PersonDTO entityToDto(PersonEntity entity) {
 		PersonDTO dto = modelMapper.map(entity, PersonDTO.class);
-	    return dto;
+		return dto;
 	}
 
 	@Override
 	public PersonEntity dtoToEntity(PersonDTO dto) {
 		return modelMapper.map(dto, PersonEntity.class);
+	}
+
+	public PersonEntity snapshotPersonalToEntity(SurveyData snapshot) {
+
+		PersonEntity pe = modelMapper.map(snapshot, PersonEntity.class);
+		if (snapshot.get("birthdate") != null) {
+			pe.setBirthdate(LocalDate.parse(snapshot.get("birthdate").toString(), DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		}
+		if(snapshot.get("countryOfBirth") != null) {
+			Optional<CountryEntity> country = countryRepository.findByAlfa2Code((snapshot.get("countryOfBirth")).toString());
+			
+			if(country.isPresent()) {
+				pe.setCountryOfBirth(country.get());
+			} else {
+				pe.setCountryOfBirth(null);
+			}
+		}
+		
+		return pe;
 	}
 
 }
