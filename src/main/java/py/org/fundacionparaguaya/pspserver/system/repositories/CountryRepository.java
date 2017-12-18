6050a83d8b7adb3eb372de5ac557d07e62ee910e
Mirta Gonzalez <mirti.gonz@gmail.com>
2017-12-17 22:35:36 -0300

    Add method to find a country

--- src/main/java/py/org/fundacionparaguaya/pspserver/system/repositories/CountryRepository.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/system/repositories/CountryRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import py.org.fundacionparaguaya.pspserver.system.entities.CountryEntity;
 
 import java.lang.Long;
+import java.util.Optional;
 
 public interface CountryRepository extends JpaRepository<CountryEntity, Long> {
-
+	Optional<CountryEntity> findByAlfa2Code(String alfa3code);
 }
