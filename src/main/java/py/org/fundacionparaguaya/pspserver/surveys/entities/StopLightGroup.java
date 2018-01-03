a63ad719dcc037ed77a29791a239be38e5cd6ede
Mirta Gonzalez <mirti.gonz@gmail.com>
2018-01-03 13:11:33 -0300

    Change Mapper: NewSnapshot to PersonEntity
    The fields of personal information are saved in a
    snapshots_properties_attributes table because these are
    field of the survey and there are fields mandatory.

--- src/main/java/py/org/fundacionparaguaya/pspserver/surveys/entities/StopLightGroup.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/surveys/entities/StopLightGroup.java
@@ -4,5 +4,5 @@ package py.org.fundacionparaguaya.pspserver.surveys.entities;
  * Created by rodrigovillalba on 10/18/17.
  */
 public enum StopLightGroup {
-    INDICATOR, ECONOMIC;
+    INDICATOR, ECONOMIC, PERSONAL;
 }
