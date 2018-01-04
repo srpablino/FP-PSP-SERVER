ec8d34cb4ce0b810654dc9ec4ab3d221984b6d12
Mirta Gonzalez <mirti.gonz@gmail.com>
2018-01-04 17:32:49 -0300

    Change attributes of SnapshotEconomic: Integer to Decimal
    The changes were made in the database and in the entity.
    In addition, the data type in the schema was changed from
    integer to number.
    
    The property validator from number was modified to accept
    values ​​without a comma.
    
    The static property mappers were modified so that it could
    convert number to Double.

--- src/main/java/py/org/fundacionparaguaya/pspserver/families/entities/PersonEntity.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/families/entities/PersonEntity.java
@@ -248,7 +248,13 @@ public class PersonEntity extends BaseEntity {
                 .stream()
                 .forEach((entry) -> {
                     try {
-                        PropertyUtils.setProperty(this, entry.getKey(), entry.getValue());
+                        Object value = null;
+                        if(Double.class.equals(PropertyUtils.getPropertyType(this, entry.getKey()))){
+                            value = Double.valueOf(entry.getValue().toString());
+                        } else {
+                            value = entry.getValue();
+                        }
+                        PropertyUtils.setProperty(this, entry.getKey(), value);
                     } catch (Exception e) {
                         throw new RuntimeException("Could not set property '" + entry.getKey() + "' to value '" + entry.getValue() + "'", e);
                     }
