e4fb9b1b08a47f97cb0d4fa474ab27e7e3162032
Marcos Céspedes <mcespedeso@gmail.com>
2018-01-08 08:51:41 -0300

    Add OPTIONAL in enum list The person may not answer a question, then an OPTIONAL option is added in the StopLightType

--- src/main/java/py/org/fundacionparaguaya/pspserver/surveys/entities/StoptLightType.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/surveys/entities/StoptLightType.java
@@ -4,5 +4,5 @@ package py.org.fundacionparaguaya.pspserver.surveys.entities;
  * Created by rodrigovillalba on 10/19/17.
  */
 public enum StoptLightType {
-    MANDATORY, RECOMMENDED;
+    MANDATORY, RECOMMENDED, OPTIONAL;
 }
