aff5843e41fdb40d9e0120584df16405e2e1178e
Mirta Gonzalez <mirti.gonz@gmail.com>
2018-01-09 18:08:15 -0300

    Add a property to the mapper
    To skip a field

--- src/main/java/py/org/fundacionparaguaya/pspserver/security/mapper/TermCondPolMapper.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/security/mapper/TermCondPolMapper.java
@@ -2,7 +2,10 @@ package py.org.fundacionparaguaya.pspserver.security.mapper;
 
 import java.util.List;
 
+//import javax.print.attribute.standard.Destination;
+
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
 import org.springframework.stereotype.Component;
 
 import py.org.fundacionparaguaya.pspserver.common.mapper.BaseMapper;
@@ -19,9 +22,20 @@ public class TermCondPolMapper implements BaseMapper<TermCondPolEntity,
     TermCondPolDTO> {
 
     private final ModelMapper modelMapper;
+    
+    PropertyMap<TermCondPolEntity, TermCondPolDTO> skip = new PropertyMap<TermCondPolEntity, TermCondPolDTO>()
+    {
+        protected void configure()
+        {
+           map(destination.getCreatedDate()).setCreatedDate(null);
+        }
+    };
 
     public TermCondPolMapper(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
+        if (skip != null) {
+            this.modelMapper.addMappings(skip);
+        }
     }
 
     @Override
@@ -33,7 +47,9 @@ public class TermCondPolMapper implements BaseMapper<TermCondPolEntity,
 
     @Override
     public TermCondPolDTO entityToDto(TermCondPolEntity entity) {
+      
         TermCondPolDTO dto = modelMapper.map(entity, TermCondPolDTO.class);
+        dto.setCreatedDate(entity.getCreatedDateAsISOString());
         return dto;
     }
 
