261dbf88daccf5aca84fe6403906226ba7dad706
Marcos Céspedes <mcespedeso@gmail.com>
2018-01-22 15:37:00 -0300

    Create table to store password reset
    Create table to store password reset and
    add email column in users table

---
+++ src/main/resources/db/changelog/sql/2018-01-22T11:31.sql
@@ -0,0 +1,19 @@
+--Add email column in security.users 
+ALTER TABLE security.users
+    ADD COLUMN email text;
+    
+--Create table to store reset password user
+CREATE TABLE security.password_reset_token
+(
+  id bigserial PRIMARY KEY,
+  token text NOT NULL,
+  user_id bigint NOT NULL,
+  expiry_date date NOT NULL,
+  temporal_password text NOT NULL
+);
+
+-- add constraint to security.password_reset_token
+ALTER TABLE security.password_reset_token ADD CONSTRAINT fk_password_reset_token_users FOREIGN KEY (user_id)
+        REFERENCES security.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION;
