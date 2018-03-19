657aacef5bc07532cd159fb732bdff59b43dc5db
Mirta Gonzalez <mirti.gonz@gmail.com>
2018-03-19 16:08:02 -0300

    Add family into datamodel

--- src/main/java/py/org/fundacionparaguaya/pspserver/surveys/entities/SnapshotEconomicEntity_.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/surveys/entities/SnapshotEconomicEntity_.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
+import py.org.fundacionparaguaya.pspserver.families.entities.FamilyEntity;
+
 /**
  * @author mgonzalez
  *
@@ -12,15 +14,18 @@ import javax.persistence.metamodel.StaticMetamodel;
 @StaticMetamodel(SnapshotEconomicEntity.class)
 public class SnapshotEconomicEntity_ {
 
-    private static volatile
-    SingularAttribute<SnapshotEconomicEntity, LocalDateTime> createdAt;
+    private static volatile SingularAttribute<SnapshotEconomicEntity, LocalDateTime> createdAt;
+    private static volatile SingularAttribute<SnapshotEconomicEntity, FamilyEntity> family;
 
     private SnapshotEconomicEntity_() {
     }
 
-    public static SingularAttribute<SnapshotEconomicEntity,
-    LocalDateTime> getCreatedAt() {
+    public static SingularAttribute<SnapshotEconomicEntity, LocalDateTime> getCreatedAt() {
         return createdAt;
+    }
+
+    public static SingularAttribute<SnapshotEconomicEntity, FamilyEntity> getFamily() {
+        return family;
     }
 
 }
