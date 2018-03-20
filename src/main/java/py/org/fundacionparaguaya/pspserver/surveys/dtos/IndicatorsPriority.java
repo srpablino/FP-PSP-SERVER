7b2d9e6e1b567a9210b26bd9114b9a5ac8b0b91c
Mirta Gonzalez <mirti.gonz@gmail.com>
2018-03-20 17:13:24 -0300

    Delete commented code

--- src/main/java/py/org/fundacionparaguaya/pspserver/surveys/dtos/IndicatorsPriority.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/surveys/dtos/IndicatorsPriority.java
@@ -19,18 +19,6 @@ public class IndicatorsPriority {
     @JsonProperty("priorities")
     private List<SnapshotIndicatorPriority> priorities = null;
     
-    /*@JsonProperty("snapshot_indicator_priority_id")
-    private Long snapshotIndicatorPriorityId;*/
-    
-   /* @JsonProperty("indicator")
-    private String indicator;
-    
-    @JsonProperty("priority")
-    private Integer priority;
-    
-    @JsonProperty("description")
-    private String description;*/
-    
     @ApiModelProperty(value = "Snapshot Indicator's id")
     public Long getSnapshotIndicatorId() {
         return snapshotIndicatorId;
@@ -48,42 +36,6 @@ public class IndicatorsPriority {
     public void setPriorities(List<SnapshotIndicatorPriority> priorities) {
         this.priorities = priorities;
     }
-    
-   /* @ApiModelProperty(value = "Snapshot Indicator Priority's id")
-    public Long getSnapshotIndicatorPriorityId() {
-        return snapshotIndicatorPriorityId;
-    }
-
-    public void setSnapshotIndicatorPriorityId(Long snapshotIndicatorPriorityId) {
-        this.snapshotIndicatorPriorityId = snapshotIndicatorPriorityId;
-    }
-
-    @ApiModelProperty(value = "The indicator that was prioritized")
-    public String getIndicator() {
-        return indicator;
-    }
-
-    public void setIndicator(String indicator) {
-        this.indicator = indicator;
-    }
-
-    @ApiModelProperty(value = "Indicator priority")
-    public Integer getPriority() {
-        return priority;
-    }
-
-    public void setPriority(Integer priority) {
-        this.priority = priority;
-    }
-
-    @ApiModelProperty(value = "Priority description")
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }*/
 
     public IndicatorsPriority snapshotIndicatorId(Long snapshotIndicatorId) {
         this.snapshotIndicatorId = snapshotIndicatorId;
@@ -95,36 +47,12 @@ public class IndicatorsPriority {
         return this;
     }
     
-   /* public SnapshotIndicatorPriority snapshotIndicatorPriorityId(Long snapshotIndicatorPriorityId) {
-        this.snapshotIndicatorPriorityId = snapshotIndicatorPriorityId;
-        return this;
-    }
-    
-    public SnapshotIndicatorPriority indicator(String indicator) {
-        this.indicator = indicator;
-        return this;
-    }
-    
-    public SnapshotIndicatorPriority priority(Integer priority) {
-        this.priority = priority;
-        return this;
-    }
-    
-    public SnapshotIndicatorPriority description(String description) {
-        this.description = description;
-        return this;
-    }*/
-    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append("class Snapshot Indicator Priority {\n");
         sb.append("    snapshotIndicatorId: ").append(toIndentedString(snapshotIndicatorId)).append("\n");
         sb.append("    priorities: ").append(toIndentedString(priorities)).append("\n");
-        /*sb.append("    snapshotIndicatorPriorityId: ").append(toIndentedString(snapshotIndicatorPriorityId)).append("\n");
-        sb.append("    indicator: ").append(toIndentedString(indicator)).append("\n");
-        sb.append("    priority:   ").append(toIndentedString(priority)).append("\n");
-        sb.append("    description:   ").append(toIndentedString(description)).append("\n");*/
         sb.append("}");
         return sb.toString();
     }
@@ -144,10 +72,7 @@ public class IndicatorsPriority {
         IndicatorsPriority that = (IndicatorsPriority) o;
 
         return com.google.common.base.Objects.equal(this.snapshotIndicatorId, that.snapshotIndicatorId) &&
-                com.google.common.base.Objects.equal(this.priorities, that.priorities); //&&
-               /* com.google.common.base.Objects.equal(this.indicator, that.indicator) &&
-                com.google.common.base.Objects.equal(this.priority, that.priority) &&
-                com.google.common.base.Objects.equal(this.description, that.description);*/
+                com.google.common.base.Objects.equal(this.priorities, that.priorities); 
     }
 
     @Override
