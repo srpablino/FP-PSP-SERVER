f29e75c7a9a826fc5edd01ee3e434817494f0a29
Mirta Gonzalez <mirti.gonz@gmail.com>
2018-03-22 16:34:36 -0300

    Fix code format

--- src/main/java/py/org/fundacionparaguaya/pspserver/reports/dtos/CsvDTO.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/reports/dtos/CsvDTO.java
@@ -2,18 +2,23 @@ package py.org.fundacionparaguaya.pspserver.reports.dtos;
 
 import java.io.Serializable;
 
-public class CsvDTO implements Serializable{
-    /**
-     * 
-     */
+/**
+ *
+ * @author mgonzalez
+ *
+ */
+public class CsvDTO implements Serializable {
+
     private static final long serialVersionUID = 1L;
+
     private String csv;
+
     public String getCsv() {
         return csv;
     }
+
     public void setCsv(String csv) {
         this.csv = csv;
     }
-    
-    
+
 }
--- src/main/java/py/org/fundacionparaguaya/pspserver/reports/dtos/ReportDTO.java
+++ src/main/java/py/org/fundacionparaguaya/pspserver/reports/dtos/ReportDTO.java
@@ -4,43 +4,36 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-//import py.org.fundacionparaguaya.pspserver.surveys.dtos.SurveyData;
-
 /**
  *
  * @author mgonzalez
  *
  */
-public class ReportDTO implements Serializable{
+public class ReportDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
-    List<String> headers;
-    List<List<String>> rows;
-    
+    private List<String> headers;
+    private List<List<String>> rows;
+
     public ReportDTO() {
-       this.headers = new ArrayList<>(); 
-       this.rows = new ArrayList<>();
+        this.headers = new ArrayList<>();
+        this.rows = new ArrayList<>();
     }
-    
+
     public List<String> getHeaders() {
         return headers;
     }
-    
+
     public void setHeaders(List<String> headers) {
         this.headers = headers;
     }
-    
+
     public List<List<String>> getRows() {
         return rows;
     }
-    
+
     public void setRows(List<List<String>> rows) {
         this.rows = rows;
     }
-    
-    
-    
-    
+
 }
-
-
