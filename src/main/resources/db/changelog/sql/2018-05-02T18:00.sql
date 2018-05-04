d8c3af93d5253add04a31031d1d4089648d4d0ec
Pablo <srpablino@gmail.com>
2018-05-04 14:37:24 -0400

    ChangeSet is used instead of individuals SQL files

--- src/main/resources/db/changelog/sql/2018-05-02T18:00.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO system.parameter(
-           parameter_id, key_parameter, description, type_parameter, value)
-   VALUES (nextval('system.parameter_parameter_id_seq'), 'minimum_priority', 'Minimum priority amount for the life map', 'Integer', 3);
